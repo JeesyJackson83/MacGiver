36c519658f06cbb250a8eec382dd8f5fac575805
JessyJackson83 <jessyjackson83@gmail.com>
2019-01-02 15:17:21 +0100

    create constant.py

---
+++ constant.py
@@ -0,0 +1,17 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#parametre de la fenetre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#personnalisation de la fenetre
+titre_fenetre = "DK Labyrinthe by JJ"
+image_icone = "images/dk_droite.png"
+
+#listes des images en jeu
+image_accueil = "images/accueil.png"
+image_fond = "images/fond.jpg"
+image_mur = "images/mur.png"
+image_depart = "images/depart.png"
+image_arrivee = "images/arrivee.png"
